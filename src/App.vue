e6e0bcdfb0191d2d5b4d6f3f2f71e3343cf7a844
TalyaSakov <talyasa@post.bgu.ac.il>
2022-06-24 15:53:32 +0300

    console log

--- src/App.vue
+++ src/App.vue
@@ -42,6 +42,7 @@ export default {
   methods: {
     async Logout() {
       // try {
+        console.log("ddddddd")
         const response = await this.axios.post(
           "http://localhost:3000/Logout"
         );
