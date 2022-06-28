46f364b55213eb2e6dbe593e0df62bc2480349de
Naama Baruch <t-nbaruch@microsoft.com>
2022-06-28 20:35:35 +0300

    search page

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,57 +1,15 @@
 <template>
   <div class="container">
     <h1 class="title">Search for our recipes..</h1>
-    <b-form @submit.prevent="onRegister">
-      <b-form-group
-        id="input-group-lastname"
-        label-cols-sm="3"
-        label="Last Name:"
-        label-for="lastname"
-      >
-        <b-form-input
-          id="lastname"
-          v-model="$v.form.lastname.$model"
-          type="text"
-          :state="validateState('lastname')"
-        >
-        </b-form-input>
-        <b-form-invalid-feedback v-if="!$v.form.lastname.required">
-          Last Name is required
-        </b-form-invalid-feedback>
-        <b-form-invalid-feedback v-if="!$v.form.lastname.alpha">
-          Last Name must contain only letters
-        </b-form-invalid-feedback>
-      </b-form-group>
-
-      <b-form-group id="input-group-cuisine" label-cols-sm="3" label="Cuisine:" label-for="cuisine">
-        <b-form-select id="cuisine" v-model="$v.form.cuisine.$model" :options="cuisine_options" ></b-form-select>
-      </b-form-group>
-
-      <b-form-group id="input-group-diet" label-cols-sm="3" label="Diet:" label-for="diet">
-        <b-form-select id="diet" v-model="$v.form.cuisine.$model" :options="diet_options" ></b-form-select>
-      </b-form-group>
-
-      <b-form-group id="input-group-intolerance" label-cols-sm="3" label="Intolerance:" label-for="intolerance">
-        <b-form-select id="intolerance" v-model="$v.form.cuisine.$model" :options="intolerance_options" ></b-form-select>
-      </b-form-group>
-
-      <b-button
-        type="submit"
-        variant="primary"
-        style="width:250px;"
-        class="ml-5 w-75"
-        >Register</b-button>
-
-    </b-form>
 
   </div>
 </template>
 
 <script>
-import {
-  required,
-  alpha,
-} from "vuelidate/lib/validators";
+// import {
+//   required,
+//   alpha,
+// } from "vuelidate/lib/validators";
 
 export default {
   name: "Register",
@@ -61,16 +19,15 @@ export default {
       cuisine_options: [],
       diet_options: [],
       intolerance_options: [],
-      form: {
-        lastname: "",
-        cuisine: null,
-        diet: null,
-        intolerance: null,
-        password: "",
-        submitError: undefined
-      },
-      errors: [],
-      validated: false
+      // form: {
+      //   query: "",
+      //   cuisine: null,
+      //   diet: null,
+      //   intolerance: null,
+      //   submitError: undefined
+      // },
+      // errors: [],
+      // validated: false
     };
   },
   async created() {
@@ -88,48 +45,23 @@ export default {
         return;
       }
   },
-  validations: {
-    form: {
-      lastname: {
-        required,
-        alpha
-      },
-      cuisine: {
-        required
-      }
-    }
-  },
-  methods: {
-    validateState(param) {
-      const { $dirty, $error } = this.$v.form[param];
-      return $dirty ? !$error : null;
-    },
-    async Register() {
-      try {
-        const response = await this.axios.post(
-          this.$root.store.server_domain + "/Register",
-
-          {
-            lastname: this.form.lastname,
-            password: this.form.password,
-            cuisine: this.form.cuisine,
-            diet: this.form.diet,
-            intolerance: this.form.intolerance,
-          }
-        );
-      } catch (err) {
-        console.log(err.response);
-        this.form.submitError = err.response.data.message;
-      }
-    },
-    onRegister() {
-      this.$v.form.$touch();
-      if (this.$v.form.$anyError) {
-        return;
-      }
-      this.Register();
-    }
-  }
+  // validations: {
+  //   form: {
+  //     query: {
+  //       required,
+  //       alpha
+  //     },
+  //     cuisine: {
+  //       required
+  //     }
+  //   }
+  // },
+  // methods: {
+  //   validateState(param) {
+  //     const { $dirty, $error } = this.$v.form[param];
+  //     return $dirty ? !$error : null;
+  //   },
+  // }
 };
 </script>
 <style lang="scss" scoped>
